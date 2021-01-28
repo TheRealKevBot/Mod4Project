ece5bd92a4794bea85b7b6ca92711ed96bc3a2c1
TheRealKevBot <Kglas001@gmail.com>
2021-01-27 22:00:32 -0700

    4th commit

--- Frontend/frontend-app/src/Components/Footer.js
+++ Frontend/frontend-app/src/Components/Footer.js
@@ -4,16 +4,16 @@ export default function Footer() {
     return (
         <footer className="footer">
             <ul className="footer-bar">
-                <li className="link">Careers</li>
-                <li className="link">Contact Us</li>
-                <li className="link">Development</li>
-                <li className="link">Forms</li>
-                <li className="link">News</li>
-                <li className="link">Privacy</li>
-                <li className="link">Security</li>
-                <li className="link">Shop</li>
-                <li className="link">Sponsors</li>
-                <li className="link">Terms</li>
+                <li className="links">Careers</li>
+                <li className="links">Contact Us</li>
+                <li className="links">Development</li>
+                <li className="links">Forms</li>
+                <li className="links">News</li>
+                <li className="links">Privacy</li>
+                <li className="links">Security</li>
+                <li className="links">Shop</li>
+                <li className="links">Sponsors</li>
+                <li className="links">Terms</li>
                 
             </ul>
         </footer>
--- Frontend/frontend-app/src/Components/PetsContainer.js
+++ Frontend/frontend-app/src/Components/PetsContainer.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import PetsCard from './PetsCard'
 
-export default function PetsContainer() {
+export default function PetsContainer(props) {
+
+    const animals = props.animals.map(animal => {
+        return <PetsCard animals={animal} />
+    })
+
     return (
-        <div>
-            
+        <div className="pets-container">
+            {animals}
         </div>
     )
 }
